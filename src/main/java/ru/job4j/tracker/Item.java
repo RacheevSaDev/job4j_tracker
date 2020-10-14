73f900f03c2494235e7eeeba5c8e039d1447ce97
racheev <racheev.sa.dev@gmail.com>
2020-10-14 19:16:36 +0700

    7. toString [#371135]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -38,4 +38,9 @@ public class Item {
     public LocalDateTime getCreated() {
         return created;
     }
+
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id + ", name='" + name + '\'' + ", created=" + created + '}';
+    }
 }
