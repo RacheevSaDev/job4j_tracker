00da2ffe3d0fd4d88f6861234769f0d1e03392ba
racheev <racheev.sa.dev@gmail.com>
2021-01-12 17:29:25 -0500

    2. Singleton. [#371103] Исправлены на композицию с классом Tracker

---
+++ src/main/java/ru/job4j/tracker/TrackerSinglePrivateStaticFinalClass.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+public class TrackerSinglePrivateStaticFinalClass {
+    private TrackerSinglePrivateStaticFinalClass() {
+    }
+
+    public static Tracker getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    public Item add(Item model) {
+        return model;
+    }
+
+    private static final class Holder {
+        private static final Tracker INSTANCE = new Tracker();
+    }
+
+    public static void main(String[] args) {
+        Tracker tracker = TrackerSinglePrivateStaticFinalClass.getInstance();
+    }
+}
--- src/main/java/ru/job4j/tracker/TrackerSingleStaticFinalField.java
+++ src/main/java/ru/job4j/tracker/TrackerSingleStaticFinalField.java
@@ -1,75 +1,16 @@
 package ru.job4j.tracker;
 
-import java.util.Arrays;
-
 public class TrackerSingleStaticFinalField {
-    private static final TrackerSingleStaticFinalField INSTANCE = new TrackerSingleStaticFinalField();
-
-    private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
+    private static final Tracker INSTANCE = new Tracker();
 
     private TrackerSingleStaticFinalField() {
     }
 
-    public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
-        return item;
+    public static Tracker getInstance() {
+        return INSTANCE;
     }
 
-    public Item findById(int id) {
-        int index = indexOf(id);
-        return index != -1 ? items[index] : null;
-    }
-
-    public Item[] findByName(String key) {
-        Item[] foundItems = new Item[this.size];
-        int foundSize = 0;
-        for (int i = 0; i < this.size; i++) {
-            Item item = items[i];
-            if (key.equals(item.getName())) {
-                foundItems[foundSize++] = item;
-            }
-        }
-        return Arrays.copyOf(foundItems, foundSize);
-    }
-
-    public Item[] findAll() {
-        return Arrays.copyOf(this.items, this.size);
-    }
-
-    public boolean replace(int id, Item item) {
-        int replaceIndex = indexOf(id);
-        if (replaceIndex != -1) {
-            item.setId(id);
-            this.items[replaceIndex] = item;
-            return true;
-        }
-        return false;
-    }
-
-    public boolean delete(int id) {
-        int index = indexOf(id);
-        if (index != -1) {
-            int start = index + 1;
-            int length = size - index;
-            System.arraycopy(this.items, start, this.items, index, length);
-            this.items[size - 1] = null;
-            size--;
-            return true;
-        }
-        return false;
-    }
-
-    private int indexOf(int id) {
-        int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
-                rsl = index;
-                break;
-            }
-        }
-        return rsl;
+    public static void main(String[] args) {
+        Tracker tracker = TrackerSingleStaticFinalField.getInstance();
     }
 }
