b4b43391b78779586988c4132b8b1844d11418d2
racheev <racheev.sa.dev@gmail.com>
2021-03-26 14:46:17 -0500

    2. Travis CI [#371031]

--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -24,6 +24,6 @@ public class OrderConvertTest {
         orders.add(new Order("3sfe", "Dress"));
         orders.add(new Order("3sfe", "Dress"));
         HashMap<String, Order> map = OrderConvert.process(orders);
-        assertEquals(map.size(),1);
+        assertEquals(map.size(), 1);
     }
 }
