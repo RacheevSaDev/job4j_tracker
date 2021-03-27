9d9aabb14b43e7fb04cce54d7741d2eec25700b6
racheev <racheev.sa.dev@gmail.com>
2021-03-27 12:28:38 -0500

    3. Компаратор для строк [#371040] переписан нормально, без перехвата исключения

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -3,22 +3,20 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 public class StringCompare implements Comparator<String> {
-
     @Override
     public int compare(String left, String right) {
-        int rsl = 0;
-        int i = 0;
-        while (rsl == 0) {
-            try {
-                char leftChar = left.charAt(i);
-                char rightChar = right.charAt(i);
-                 rsl = Character.compare(leftChar, rightChar);
-            } catch (IndexOutOfBoundsException e) {
-                rsl = Integer.compare(left.length(), right.length());
-                break;
+
+        int leftSize = left.length();
+        int rightSize = right.length();
+        int minSize = Integer.min(leftSize, rightSize);
+        for (int i = 0; i < minSize; i++) {
+            char leftChar = left.charAt(i);
+            char rightChar = right.charAt(i);
+            int resCompare = Character.compare(leftChar, rightChar);
+            if (resCompare != 0) {
+                return (resCompare);
             }
-            i++;
         }
-        return rsl;
+        return Integer.compare(leftSize, rightSize);
     }
 }
