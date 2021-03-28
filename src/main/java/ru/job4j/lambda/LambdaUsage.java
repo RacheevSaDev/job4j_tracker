316c6cdbebe68fea07dcee57b813f6af63241f51
racheev <racheev.sa.dev@gmail.com>
2021-03-27 20:41:53 -0500

    2.2. Лямбда блок [#370873]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<Attachment> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.getSize() + " : " + left.getSize());
+            return right.getSize() - left.getSize();
+        };
+        Attachment att1 = new Attachment("att1", 100);
+        Attachment att2 = new Attachment("att2", 120);
+        cmpDescSize.compare(att1, att2);
+    }
+}
