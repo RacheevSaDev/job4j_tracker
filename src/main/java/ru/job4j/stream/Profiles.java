69365a47f64effcdbc35a327d7737c00b49a47ea
racheev <racheev.sa.dev@gmail.com>
2021-04-19 21:25:45 -0500

    2. Список адресов [#370882] расчёт стрима сделан в одну строчку

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,15 +1,10 @@
 package ru.job4j.stream;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        List<Address> res;
-        res = profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
-        res.sort(Comparator.comparing(Address::getCity));
-        res = res.stream().distinct().collect(Collectors.toList());
-        return res;
+        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
     }
 }
