ebe2003ff0a55650ac74b4059d8a7467cc04ba3e
racheev <racheev.sa.dev@gmail.com>
2021-06-09 13:01:43 -0500

    5. Преобразование матрицы чисел в список чисел. [#110228 #165815] переделан метод convert

--- src/main/java/ru/job4j/stream/Array2ToFlatArray.java
+++ src/main/java/ru/job4j/stream/Array2ToFlatArray.java
@@ -1,13 +1,12 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Array2ToFlatArray {
 
-    ArrayList<Integer> convert(Integer[][] array2) {
-        return (ArrayList<Integer>)
-                Stream.of(array2).flatMap(Stream::of).collect(Collectors.toList());
+    public List<Integer> convert(Integer[][] array2) {
+        return Stream.of(array2).flatMap(Stream::of).collect(Collectors.toList());
     }
 }
