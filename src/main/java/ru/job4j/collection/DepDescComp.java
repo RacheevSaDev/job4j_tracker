9a0c842c9ae48e08dc3134450aef147ddb81db1d
racheev <racheev.sa.dev@gmail.com>
2021-06-14 15:41:41 -0500

    2. Отсортировать департаменты [#34352 #167449] говнокод переделан в приличный компоратор

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,20 +7,11 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] o1Split = o1.split("/");
         String[] o2Split = o2.split("/");
-        if (o1Split[0].equals(o2Split[0])) {
-            if (o1Split.length == o2Split.length) {
-                for (int i = 1; i < o1Split.length; i++) {
-                    int rsl = o1Split[i].compareTo(o2Split[i]);
-                    if (rsl != 0) {
-                        return rsl;
-                    }
-                }
-            } else {
-                return Integer.compare(o1Split.length, o2Split.length);
-            }
+        int rsl = o2Split[0].compareTo(o1Split[0]);
+        if (rsl == 0) {
+            return o1.compareTo(o2);
         } else {
-            return o2Split[0].compareTo(o1Split[0]);
+            return rsl;
         }
-        return 0;
     }
 }
