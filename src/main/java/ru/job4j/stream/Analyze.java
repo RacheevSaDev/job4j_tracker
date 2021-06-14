3fe7559e6445d8a31a029f3fbc68c9e65e006b56
racheev <racheev.sa.dev@gmail.com>
2021-06-14 02:16:52 -0500

    2. Аттестация [#49777 #167422] используется LinkedHashMap, чтобы порядок элементов был не важен

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,7 +1,9 @@
 package ru.job4j.stream;
 
-import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -28,6 +30,7 @@ public class Analyze {
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
         return stream.flatMap(pupil -> pupil.getSubjects().stream())
                 .collect(Collectors.groupingBy(Subject::getName,
+                        LinkedHashMap::new,
                         Collectors.averagingDouble(Subject::getScore)))
                 .entrySet()
                 .stream()
@@ -43,7 +46,6 @@ public class Analyze {
                                                       .mapToInt(s -> s).sum()))
                 .max((o1, o2) -> Double.compare(o1.getScore(), o2.getScore()))
                 .orElse(null);
-
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
