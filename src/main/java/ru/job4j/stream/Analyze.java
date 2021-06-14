9bd96ca93561c48504b373ffc94ebcbce423285d
racheev <racheev.sa.dev@gmail.com>
2021-06-14 02:18:35 -0500

    2. Аттестация [#49777 #167422] используется LinkedHashMap, чтобы порядок элементов был не важен Убраны неиспользуемые пакеты

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -2,8 +2,6 @@ package ru.job4j.stream;
 
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
